5486791b592b76c11007577195914f4fd7f17f56
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2024-08-30 22:23:42 +0100

    Add CartProduct.remove and CartProduct.initActions methods

--- dist/js/script.js
+++ dist/js/script.js
@@ -467,6 +467,7 @@
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
     }
 
     getElements(element) {
@@ -500,7 +501,31 @@
         //Updating the price in the DOM element
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
       });
+    }
 
+    remove() {
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
+
+    initActions() {
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener('click', (event)=> {
+        event.preventDefault();
+      });
+
+      thisCartProduct.dom.remove.addEventListener('click', (event)=> {
+        event.preventDefault();
+        thisCartProduct.remove();
+      });
     }
   }
 
