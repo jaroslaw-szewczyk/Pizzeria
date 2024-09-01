555197b43f02d30804b216c92cd502487582fce9
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2024-09-01 14:08:27 +0100

    Add Cart.sendOrder method and CartProduct.getData method

--- src/js/script.js
+++ src/js/script.js
@@ -400,6 +400,9 @@
       thisCart.dom.subtotalPrice = thisCart.dom.wrapper.querySelector(select.cart.subtotalPrice);
       thisCart.dom.totalPrice = thisCart.dom.wrapper.querySelectorAll(select.cart.totalPrice);
       thisCart.dom.totalNumber = thisCart.dom.wrapper.querySelector(select.cart.totalNumber);
+      thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+      thisCart.dom.phone = thisCart.dom.form.querySelector(select.cart.phone);
+      thisCart.dom.address = thisCart.dom.form.querySelector(select.cart.address);
     }
 
     initActions() {
@@ -413,9 +416,14 @@
         thisCart.update();
       });
 
-      thisCart.dom.productList.addEventListener('remove', (event)=>{
+      thisCart.dom.productList.addEventListener('remove', event=>{
         thisCart.remove(event.detail.cartProduct);
       });
+
+      thisCart.dom.form.addEventListener('submit', event=>{
+        event.preventDefault();
+        thisCart.sendOrder();
+      })
     }
 
     add(menuProduct) {
@@ -436,31 +444,31 @@
 
       let deliveryFee = settings.cart.defaultDeliveryFee;
 
-      let totalNumber = 0;
-      let subtotalPrice = 0;
+      thisCart.totalNumber = 0;
+      thisCart.subtotalPrice = 0;
       
       for(const product of thisCart.products){
-        totalNumber += product.amount;
-        subtotalPrice += product.price;
+        thisCart.totalNumber += product.amount;
+        thisCart.subtotalPrice += product.price;
       }
       
-      if(totalNumber === 0) {
+      if(thisCart.totalNumber === 0) {
         thisCart.totalPrice = 0;
         thisCart.dom.deliveryFee.innerHTML = 0;
       } else {
-        thisCart.totalPrice = subtotalPrice + deliveryFee;
+        thisCart.totalPrice = thisCart.subtotalPrice + deliveryFee;
         thisCart.dom.deliveryFee.innerHTML = deliveryFee;
       }
 
-      thisCart.dom.totalNumber.innerHTML = totalNumber;
-      thisCart.dom.subtotalPrice.innerHTML = subtotalPrice;
+      thisCart.dom.totalNumber.innerHTML = thisCart.totalNumber;
+      thisCart.dom.subtotalPrice.innerHTML = thisCart.subtotalPrice;
       
       for(const totalCost of thisCart.dom.totalPrice){
         totalCost.innerHTML = thisCart.totalPrice;
       }
     }
 
-    remove(product){
+    remove(product) {
       const thisCart = this;
       
       //Searching for an index in an array of products
@@ -474,6 +482,42 @@
       
       //basket update
       thisCart.update();
+    }
+
+    sendOrder() {
+      const thisCart = this;
+
+      const url = settings.db.url + '/' + settings.db.orders;
+
+      const payload = {
+        address: thisCart.dom.address.value,
+        phone: thisCart.dom.phone.value,
+        totalPrice: thisCart.totalPrice,
+        subtotalPrice: thisCart.subtotalPrice,
+        totalNumber: thisCart.totalNumber,
+        deliveryFee: settings.cart.defaultDeliveryFee,
+        products: []
+      }
+
+      for(const prod of thisCart.products) {
+        payload.products.push(prod.getData());
+        console.log('prod', prod);
+      }
+
+      const options = {
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload)
+      }
+
+      fetch(url, options)
+        .then(response => {
+          return response.json();
+        }).then(parsedResponse => {
+          console.log('parsedResponse',parsedResponse);
+        })
     }
   }
 
@@ -490,6 +534,8 @@
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
       thisCartProduct.initActions();
+
+      // console.log('thisCartProduct', thisCartProduct);
     }
 
     getElements(element) {
@@ -549,6 +595,22 @@
         thisCartProduct.remove();
       });
     }
+
+    getData() {
+      const thisCartProduct = this;
+
+      const product = {
+        id: thisCartProduct.id,
+        amount: thisCartProduct.amount,
+        price: thisCartProduct.price,
+        priceSingle: thisCartProduct.priceSingle,
+        name: thisCartProduct.name,
+        params: thisCartProduct.params,
+      }
+
+      return product;
+    }
+
   }
 
   const app = {
